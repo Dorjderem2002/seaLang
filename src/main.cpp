9cc1e66434f2ea86bcd905c8f23124413adf6992
Dorjderem2002 <dorjderemdorj202@gmail.com>
2023-11-14 03:08:12 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+#include <fstream>
 #include <iomanip>
 
 using namespace std;
 
-int DisassembleOp8080(unsigned char *codebuffer, int pc)
+int DisassembleOp8080(vector<uint8_t> codebuffer, int pc)
 {
-    unsigned char *code = &codebuffer[pc];
+    vector<uint8_t>::iterator code = codebuffer.begin() + pc;
     int opbytes = 1;
     cout << pc << " ";
     switch (*code)
     {
     case 0x00:
+        cout << "NOP";
         break;
     case 0x01:
         cout << "LXI B,#$" << code[2] << code[1];
@@ -29,24 +31,55 @@ int DisassembleOp8080(unsigned char *codebuffer, int pc)
         cout << "DCR B";
         break;
     case 0x06:
-        cout<<"MVI B,#$"<<code[1];  
+        cout << "MVI B,#$" << code[1];
         opbytes = 2;
         break;
     case 0x07:
-        printf("RLC");
+        cout << "RLC";
         break;
     case 0x08:
-        printf("NOP");
+        cout << "NOP";
         break;
+    case 0x09:
+        cout << "DAD B";
+        break;
+    case 0x0a:
+        cout << "LDAX B";
+        break;
+    case 0x0b:
+        cout << "DCX B";
+        break;
+    case 0x0c:
+        cout << "INR C";
+        break;
+    case 0x0d:
+        cout << "DCR C";
+    case 0x0e:
+        cout << "MVI " << code[1] << ",D8";
     default:
         break;
     }
-    cout<<"\n";
+    cout << "\n";
     return opbytes;
 }
 
 int main()
 {
-    cout << "Hello 8080" << endl;
+    ifstream program;
+    program.open("invaders");
+    string line;
+    vector<uint8_t> s;
+    int t;
+    while (program >> hex >> t)
+    {
+        s.push_back((uint8_t)t);
+    }
+    program.close();
+    int pc = 0;
+    while (pc < (int)s.size())
+    {
+        pc += DisassembleOp8080(s, pc);
+    }
+
     return 0;
 }
